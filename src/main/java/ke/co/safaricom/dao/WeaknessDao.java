3be22be46850f81e16d31ad967bbd9513687548e
Julius Ojakaa <ojayallday@gmail.com>
2023-12-07 00:20:48 +0300

    Model classes initial files set up

--- src/main/java/ke/co/safaricom/dao/WeaknessDao.java
+++ src/main/java/ke/co/safaricom/dao/WeaknessDao.java
@@ -1,4 +1,12 @@
 package ke.co.safaricom.dao;
 
+import org.crotich.config.DatabaseConfig;
+import ke.co.safaricom.models.Weakness;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeaknessDao {
 }
