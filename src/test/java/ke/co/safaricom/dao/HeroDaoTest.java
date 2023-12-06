f40d2808c0df5cd60c7ba3e76ff1baae458d8f01
Julius Ojakaa <ojayallday@gmail.com>
2023-12-07 01:54:39 +0300

    HeroDao tests

--- src/test/java/ke/co/safaricom/dao/HeroDaoTest.java
+++ src/test/java/ke/co/safaricom/dao/HeroDaoTest.java
@@ -1,12 +1,55 @@
 package ke.co.safaricom.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
 import ke.co.safaricom.config.DatabaseConfig;
 import ke.co.safaricom.models.Hero;
-//import ke.co.safaricom
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-import java.util.ArrayList;
-import java.util.List;
 class HeroDaoTest {
+
+    private static final Sql2o sql2o = DatabaseConfig.getDatabase();
+
+    private static Connection connection = sql2o.open();
+    Hero hero=null;
+
+    @BeforeEach
+    void setUp() {
+        hero =new Hero("Ugaliman", 29, 1, 1,1);
+    }
+
+    @Test
+    @DisplayName(value = "Create Hero")
+    void createHeroTest() {
+
+        String query = "INSERT INTO hero ( name,age, squad_id, weakness_id, strength_id) VALUES (:name,:age, :squad_id, :weakness_id, :strength_id);";
+        connection.createQuery(query)
+                .addParameter("age", hero.getAge())
+                .addParameter("name", hero.getName())
+                .addParameter("squad_id", hero.getSquadIid())
+                .addParameter("weakness_id", hero.getWeaknessId())
+                .addParameter("strength_id", hero.getStrengthId())
+                .executeUpdate();
+
+        String queryB = "SELECT * FROM hero WHERE NOT deleted AND name = :name;";
+        Hero hero1 = connection.createQuery(queryB)
+                .addParameter("name", hero.getName())
+                .executeAndFetchFirst(Hero.class);
+
+        assertEquals(45,hero1.getAge());
+        assertNotEquals(70,hero1.getAge());
+    }
+
+    @AfterEach
+    void tearDown() {
+        String query = "DELETE FROM hero WHERE name = :name;";
+        connection.createQuery(query)
+                .addParameter("name", hero.getName())
+                .executeUpdate();
+
+    }
 
 }
