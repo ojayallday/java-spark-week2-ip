2c6bdf0650f9c119bca5e8f13da5739277882fc2
Julius Ojakaa <ojayallday@gmail.com>
2023-12-06 22:29:10 +0300

    template folder creation for hbs files additions

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,19 +1,118 @@
-package ke.co.safaricom;
+package org.crotich;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.crotich.dao.HeroDao;
+import org.crotich.dao.SquadDao;
+import org.crotich.dao.StrengthDao;
+import org.crotich.dao.WeaknessDao;
+import org.crotich.models.Hero;
+import org.crotich.models.Squad;
+import org.crotich.models.Strength;
+import org.crotich.models.Weakness;
+import org.crotich.utils.SharedUtils;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        staticFileLocation("/public");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        get("/", (req, res)->{
+            return SharedUtils.render(new HashMap<>(), "index.hbs");
+        });
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        get("/strength/add", (req, res)->{
+            return SharedUtils.render(new HashMap<>(), "create-strength.hbs");
+        });
+        get("/weakness/add", (req, res)->{
+            return SharedUtils.render(new HashMap<>(), "create-weakness.hbs");
+        });
+        get("/squad/add", (req, res)->{
+            return SharedUtils.render(new HashMap<>(), "create-squad.hbs");
+        });
+        get("/hero/add", (req, res)->{
+            return SharedUtils.render(new HashMap<>(), "create-hero.hbs");
+        });
+
+        post("/create-strength", (req, res)->{
+
+            String name = req.queryParams("name");
+            System.out.println(name);
+            double score = Double.valueOf(req.queryParams("score"));
+
+            Strength strength = new Strength();
+            strength.setScore(score);
+            strength.setName(name);
+
+            StrengthDao.create(strength);
+
+            res.redirect("/");
+            return null;
+        });
+
+        post("/create-weakness", (req, res)->{
+
+            String name = req.queryParams("name");
+            System.out.println(name);
+            double score = Double.valueOf(req.queryParams("score"));
+            if(score>0){
+                score=-1* score;
+            }
+
+            Weakness weakness = new Weakness();
+            weakness.setScore(score);
+            weakness.setName(name);
+
+            WeaknessDao.create(weakness);
+
+            res.redirect("/");
+            return null;
+        });
+
+
+        post("/create-hero", (req, res)->{
+            try{
+                String name = req.queryParams("name");
+                int age = Integer.valueOf(req.queryParams("age"));
+                String squadName= req.queryParams("squadname");
+                String strengthName=req.queryParams("strengthname");
+                String weaknessName=req.queryParams("weaknessname");
+                System.out.println(weaknessName);
+                System.out.println(strengthName);
+                Squad squad =SquadDao.findSquadByName(squadName);
+                int  squadId=squad.getId();
+                int size=HeroDao.findAllHeroesBySquadId(squadId).size();
+                if(size>=squad.getMaxSize()){
+                    throw new IllegalArgumentException("squad size has been exceeded");
+                }
+
+                int strengthId=StrengthDao.findStrengthByName(strengthName);
+                System.out.println(strengthId);
+                int weaknessId=WeaknessDao.findWeaknessByName(weaknessName);
+                System.out.println(weaknessId);
+                Hero hero = new Hero(name, age, strengthId,weaknessId, squadId);
+                HeroDao.create(hero);
+            }
+            catch(Exception e){
+                System.out.print(e.getMessage());
+            }
+            res.redirect("/");
+            return null;
+        });
+        post("/create-squad", (req, res)->{
+
+            String name = req.queryParams("name");
+            int maxSize = Integer.valueOf(req.queryParams("maxSize"));
+            String cause= req.queryParams("cause");
+            Squad squad= new Squad(maxSize, name,cause);
+            SquadDao.createSquad(squad);
+            res.redirect("/");
+            return null;
+        });
+
+        System.out.print(SquadDao.findSquadById(2));
+        StrengthDao.getScoreById(4);
+
     }
 }
